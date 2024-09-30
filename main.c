d45ab0fa282839ed82614651a38cb7d23da52be1
Yuvraj <fcyuvraj18@gmail.com>
2024-09-30 23:22:41 +0530

    .

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 int main() {
-    // printf("Hello World\n");
+    printf("Hello World\n");
     // printf("%zu\n", sizeof(int));
     // printf("%zu\n", sizeof(char));
     // printf("%zu/n", sizeof(float));
     int a;
     scanf("%d", &a);
-    printf("the value ofdf a is %d", a); 
+    printf("the value of a is %d", a); 
     return 0;
 }
 
