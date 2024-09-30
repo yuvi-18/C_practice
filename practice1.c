165b93a26804324b4563f5913bc50102626b0da0
Yuvraj <fcyuvraj18@gmail.com>
2024-10-01 00:58:37 +0530

    update

---
+++ practice1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    // int length;
+    // int breath;
+    // scanf("length");
+    // scanf("breath");
+
+    int length, breadth;
+    
+    printf("Enter length\n");
+    scanf("%d", &length);
+
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+
+    printf("the are of the reactangle is ", length*breath);
+    return 0;
+}
