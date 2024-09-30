4352586cc8bf9fd9ac004378eaa6085db2d890ca
Yuvraj <fcyuvraj18@gmail.com>
2024-09-30 23:42:33 +0530

    .

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     printf("Hello World\n");
     // printf("%zu\n", sizeof(int));
     // printf("%zu\n", sizeof(char));
     // printf("%zu/n", sizeof(float));
-    int a;
-    scanf("%d", &a);
-    printf("the value of a is %d", a); 
+    // int a;
+    // scanf("%d", &a);
+    // printf("the value of a is %d", a);
+
+    int a = 0, b = 1, c = 2, d = 3, e = 4;
+    a = b - c + d * e;
+    printf("%d", a); /* will print 1-2+3*4 = 11 */
     return 0;
 }
 
@@ -15,22 +20,22 @@ int main() {
 
 //  int main()
 //  {
- 	// First program
- 	
- 	// printf("hello world\n");
- 	// printf("i am learning c\n");
- 	
- 	// Second program
- 	
-    // int myNum = 5.888; // integer
-    // int 2 or 4 bytes stores whole number, whithout decimals 
-    // float my = 9.548755523980;
-    
-    // printf("%d\n", myNum);
-    // printf("%f\n", my);
-    // return 0;
+// First program
+
+// printf("hello world\n");
+// printf("i am learning c\n");
+
+// Second program
+
+// int myNum = 5.888; // integer
+// int 2 or 4 bytes stores whole number, whithout decimals
+// float my = 9.548755523980;
+
+// printf("%d\n", myNum);
+// printf("%f\n", my);
+// return 0;
 //  }
- 
- // this is a single line comment
- /* this is a multi line comment 
- but i don't want to write  any comment fk this shit I am out*/
+
+// this is a single line comment
+/* this is a multi line comment
+but i don't want to write  any comment fk this shit I am out*/
