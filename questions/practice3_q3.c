ad04a82e89699785b44df09331cb0391e98ed344
Yuvraj <fcyuvraj18@gmail.com>
2024-10-01 19:22:45 +0530

    practice set q2 and 3

---
+++ questions/practice3_q3.c
@@ -0,0 +1,19 @@
+/*
+Calculate income tax paid by an employee to the government as per the slabs
+mentioned below:
+ Income Slab Tax
+ 2.5 – 5.0L 5%
+ 5.0L - 10.0L 20%
+ Above 10.0L 30%
+Note that there is no tax below 2.5L. Take income amount as an input from the user
+*/
+
+#include <stdio.h>
+
+int main(){
+    int i;
+    printf("Enter your income: ");
+    scanf("%d", &i);
+    
+    return 0;
+}
