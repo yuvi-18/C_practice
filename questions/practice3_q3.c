c82e27ca68cd801d87b39519b4b059dc58ab9c6c
Yuvraj <fcyuvraj18@gmail.com>
2024-10-01 20:00:15 +0530

    practice set 3 q3

--- questions/practice3_q3.c
+++ questions/practice3_q3.c
@@ -10,10 +10,34 @@ Note that there is no tax below 2.5L. Take income amount as an input from the us
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int i;
-    printf("Enter your income: ");
+    float t;
+    printf("Enter your income: \n");
     scanf("%d", &i);
-    
+
+    if (i < 250000)
+    {
+        printf("Lol You are tax free \n");
+    }
+    else if (i < 500000)
+    {
+        t = (5.0 / 100.0) * i;
+        float r = i - t;
+        printf("Your income is %d \nand the tax on it is %.2f\nso the total income after taxxes is %.2f \n", i, t, r);
+    }
+    else if (i < 1000000)
+    {
+        t = (20.0 / 100.0) * i;
+        float r = i - t;
+        printf("Your income is %d \nand the tax on it is %.2f\nso the total income after taxxes is %.2f \n", i, t, r);
+    }
+    else
+    {
+        t = (30.0 / 100.0) * i;
+        float r = i - t;
+        printf("Your income is %d \nand the tax on it is %.2f\nso the total income after taxxes is %.2f \n", i, t, r);
+    }
     return 0;
 }
--- questions/quiz1.c
+++
@@ -1,9 +0,0 @@
-/*
-Quick Quiz: Write a program to find grade of a student given his marks based on below:
-90 – 100 => A
-80 – 90 => B
-70 – 80 => C
-60 – 70 => D
-50 – 60 => E
-<50 => F
-*/
