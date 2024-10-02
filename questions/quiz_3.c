553f880b3986b0e73abf5282cf8e6768d1746b46
Yuvraj <fcyuvraj18@gmail.com>
2024-10-03 02:55:14 +0530

    quiz 3

---
+++ questions/quiz_3.c
@@ -0,0 +1,29 @@
+/*
+Quick Quiz: Write a program to print first ‘n’ natural numbers using for loop
+
+#include <stdio.h>
+
+int main(){
+    for (int i = 1; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+*/
+
+/*
+Quick Quiz: Write a program to print ‘n’ natural numbers in reverse order.
+*/
+
+#include <stdio.h>
+
+int main(){
+    for (int i = 10; i; i--)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
