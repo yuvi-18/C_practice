ca7c7f0f269401478b6734712cf2a3b75843abb4
Yuvraj <fcyuvraj18@gmail.com>
2024-10-02 17:02:32 +0530

    q5

--- questions/practice3_q5.c
+++ questions/practice3_q5.c
@@ -3,3 +3,22 @@
 lowercase or not.
 */
 
+#include <stdio.h>
+
+// ASCII value of a = 97 & ASCII value of z = 122;
+
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (ch >= 'a' && ch <= 'z')
+    {
+        printf("The character %c is a lower case character", ch);
+    }
+    else
+    {
+        printf("The charcter %c is not a lowercase character", ch);
+    }
+    
+    return 0;
+}
---
+++ questions/practice3_q6.c
@@ -0,0 +1,2 @@
+// 6. Write a program to find greatest of four numbers entered by the user
+
