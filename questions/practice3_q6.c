92939fef4ee6ac8d2cde3b304100683a44f6a31f
Yuvraj <fcyuvraj18@gmail.com>
2024-10-03 00:22:36 +0530

    q6 p3

--- questions/practice3_q6.c
+++ questions/practice3_q6.c
@@ -3,6 +3,24 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a, b, c, d;
+    printf("Enter Four Numbers: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("Your Entered numbers are %d, %d, %d, %d\n", a, b ,c, d);
+    if (a>b && a>c && a>d)
+    {
+       printf("%d is The greatest among the four entered numbers", a);
+    }
+    else if (b>a && b>c && b>d)
+    {
+       printf("%d is The greatest among the four entered numbers", b);
+    }
+    else if (c>a && c>b && c>d)
+    {
+       printf("%d is The greatest among the four entered numbers", c);
+    }
+    else{
+       printf("%d is The greatest among the four entered numbers", d);
+    }
     return 0;
 }
